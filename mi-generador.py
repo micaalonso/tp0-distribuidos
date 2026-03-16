6d2348fc0320db7a309bab1d142bb106f4320d9d
MicaelaAlonso <malonso@fi.uba.ar>
2026-03-15 21:00:29 -0300

    Agrego generar-compose.sh y mi-generador.py para poder crear n cantidad de contenedores para n clientes
    Se agrega un script generar-compose.sh, el cual llama al subscript mi-generador.py. En mi-generador.py hay una función llamada generate_output_file, la cual genera un archivo con los diferentes contenedores a levantar (un servidor y n cantidad de clientes)

---
+++ mi-generador.py
@@ -0,0 +1,49 @@
+import sys
+
+def generate_output_file(output_filename, amount_clients):
+    compose = f"""name: tp0
+services:
+  server:
+    container_name: server
+    image: server:latest
+    entrypoint: python3 /main.py
+    environment:
+      - PYTHONUNBUFFERED=1
+      - LOGGING_LEVEL=DEBUG
+    networks:
+      - testing_net
+"""
+    for i in range(1, amount_clients + 1):
+        compose += f"""
+  client{i}:
+    container_name: client{i}
+    image: client:latest
+    entrypoint: /client
+    environment:
+      - CLI_ID={i}
+      - CLI_LOG_LEVEL=DEBUG
+    networks:
+      - testing_net
+    depends_on:
+      - server
+"""
+    
+    compose += """
+networks:
+  testing_net:
+    ipam:
+      driver: default
+      config:
+        - subnet: 172.25.125.0/24
+"""
+
+    with open(output_filename, "w") as f:
+        f.write(compose)
+    
+    print(f"Archivo de salida con el nombre {output_filename} generado con {amount_clients} cantidad de clientes.")
+
+if __name__ == "__main__":
+    output_filename = sys.argv[1]
+    amount_clients = int(sys.argv[2])
+    
+    generate_output_file(output_filename, amount_clients)
