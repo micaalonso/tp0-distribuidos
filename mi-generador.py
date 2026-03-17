e0d516185a9176269331bc0179bc0439696f9e83
MicaelaAlonso <malonso@fi.uba.ar>
2026-03-16 21:14:08 -0300

    Agrego configuración de volumes en mi-generador.py

--- mi-generador.py
+++ mi-generador.py
@@ -6,6 +6,8 @@ services:
   server:
     container_name: server
     image: server:latest
+    volumes: 
+      - ./server/config.ini:/config.ini
     entrypoint: python3 /main.py
     environment:
       - PYTHONUNBUFFERED=1
@@ -18,6 +20,8 @@ services:
   client{i}:
     container_name: client{i}
     image: client:latest
+    volumes: 
+      - ./client/config.yaml:/config.yaml
     entrypoint: /client
     environment:
       - CLI_ID={i}
