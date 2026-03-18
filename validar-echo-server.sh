06488ffdaada6d1bfc676aaf16390c3b6a364077
MicaelaAlonso <malonso@fi.uba.ar>
2026-03-17 21:00:01 -0300

    Agrego script validar-echo-server.sh
    El script validar-echo-server.sh crea un contenedor, el cual se conecta a la misma network en la que esta el server (network especificada en el archivo docker-compose-dev.yaml). Despues este contenedor le envía al server el mensaje "hello server", y espera la respuesta del server, en caso que la respuesta sea igual al mensaje => el test funcionó, sino falló

---
+++ validar-echo-server.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+MESSAGE="hello server"
+RESPONSE=$(docker run --rm --network=tp0_testing_net alpine /bin/sh -c "echo '$MESSAGE' | nc server 12345")
+
+if [ "$RESPONSE" == "$MESSAGE" ]; then
+    echo "action: test_echo_server | result: success"
+else
+    echo "action: test_echo_server | result: fail"
+fi
