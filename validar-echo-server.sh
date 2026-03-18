b44da8e8b166463b101bdac20ec81217e680e7c9
MicaelaAlonso <malonso@fi.uba.ar>
2026-03-17 21:42:30 -0300

    Fix del script
    Cambio de identación y cambio == por -eq

--- validar-echo-server.sh
+++ validar-echo-server.sh
@@ -2,8 +2,8 @@
 MESSAGE="hello-server"
 RESPONSE=$(docker run --rm --network=tp0_testing_net alpine /bin/sh -c "echo '$MESSAGE' | nc server 12345")
 
-if [ "$RESPONSE" == "$MESSAGE" ]; then
-    echo "action: test_echo_server | result: success"
+if [ "$RESPONSE" -eq "$MESSAGE" ]; then
+  echo "action: test_echo_server | result: success"
 else
-    echo "action: test_echo_server | result: fail"
+  echo "action: test_echo_server | result: fail"
 fi
