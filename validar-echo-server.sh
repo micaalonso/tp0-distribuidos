37f3f3366baf00a0e63740369b470b0e46b50afb
MicaelaAlonso <malonso@fi.uba.ar>
2026-03-17 22:01:22 -0300

    Update validar-echo-server.sh

--- validar-echo-server.sh
+++ validar-echo-server.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
-MESSAGE="hello-server"
+MESSAGE="hello"
 RESPONSE=$(docker run --rm --network=tp0_testing_net alpine /bin/sh -c "echo '$MESSAGE' | nc server 12345")
 
-if [ "$RESPONSE" == "$MESSAGE" ]; then
+if [ "$RESPONSE" = "$MESSAGE" ]
+then
   echo "action: test_echo_server | result: success"
 else
   echo "action: test_echo_server | result: fail"
