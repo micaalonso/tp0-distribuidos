6a1c5d5ea6f7f1a9e864755a1f9ee6235626c9eb
MicaelaAlonso <malonso@fi.uba.ar>
2026-03-24 02:16:02 -0300

    Modificación de la configuración de los contenedores de los clientes para que levanten el archivo csv en volumes

--- mi-generador.py
+++ mi-generador.py
@@ -21,6 +21,7 @@ services:
     image: client:latest
     volumes: 
       - ./client/config.yaml:/config.yaml
+      - ./.data/agency-{i}.csv:/agency.csv
     entrypoint: /client
     environment:
       - CLI_ID={i}
